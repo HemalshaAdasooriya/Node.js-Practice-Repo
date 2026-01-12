4c8d96159d309b4e5cfd48656a88d11e9da3e818
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2026-01-12 23:38:48 +0530

    add: implement mark evaluation logic in mark.js

---
+++ mark.js
@@ -0,0 +1,13 @@
+const mark = "S"; // Example mark
+
+if (mark === "A") {
+    console.log("Excellent");
+}else if (mark === "B") {
+    console.log("very Good");
+}else if (mark === "C") {
+    console.log("Good");
+}else if (mark === "S") {
+    console.log("Pass");
+}else {
+    console.log("Fail");
+}
