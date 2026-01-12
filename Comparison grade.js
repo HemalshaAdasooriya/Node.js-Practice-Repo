f7eab7a4a10b69a6225e14362584f61597340991
Hemalsha Adasooriya <hemalsha2022uni@gmail.com>
2026-01-12 23:28:47 +0530

    add: create Comparison grade.js and comparison.js for grade evaluation and value comparison

---
+++ Comparison grade.js
@@ -0,0 +1,13 @@
+let mark = 68; // Example mark
+
+if (mark >= 74) {
+    console.log("Grade: A");
+}else if (mark >= 65) {
+    console.log("Grade: B");
+}else if (mark >= 50) {
+    console.log("Grade: C");
+}else if (mark >= 35) {
+    console.log("Grade: D");
+}else {
+    console.log("Grade: F");
+}
---
+++ comparison.js
@@ -0,0 +1,16 @@
+const x = 10;
+const y = '10';
+
+if(x===y){
+    console.log('x is equal to y');
+}else{
+    console.log('x is not equal to y');
+}
+
+if(x>y){
+    console.log('x is greater than y');
+}else if(x<y){
+    console.log('x is less than y');
+}  else{
+    console.log('x is equal to y');
+}
